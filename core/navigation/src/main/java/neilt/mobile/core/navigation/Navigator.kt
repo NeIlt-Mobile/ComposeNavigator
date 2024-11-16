b31674a9cdd117e9f8ca2adceab29c63adac10d4
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-11-16 07:55:14 +0300

    refactor(navigation): move AndroidNavigator and remove internal modifier
    - Transferred `AndroidNavigator` from `internal` package to make it publicly accessible.
    - Removed `internal` modifier to allow dependency injection and broader usability.
    - Enhanced `AndroidNavigator` with logging for failed navigation actions.
    - Simplified navigation management by ensuring duplicate actions are not reprocessed.

--- core/navigation/src/main/java/neilt/mobile/core/navigation/Navigator.kt
+++ core/navigation/src/main/java/neilt/mobile/core/navigation/Navigator.kt
@@ -1,9 +1,12 @@
 package neilt.mobile.core.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.onFailure
 import kotlinx.coroutines.flow.Flow
-import neilt.mobile.core.navigation.internal.AndroidNavigator
+import kotlinx.coroutines.flow.receiveAsFlow
 
 /**
  * Interface for defining navigation behavior within an application.
@@ -28,6 +31,59 @@ interface Navigator {
      */
     suspend fun navigateUp()
 }
+
+/**
+ * Implementation of [Navigator] for Android platform.
+ *
+ * This class handles navigation actions and provides a flow of [NavigationAction] events.
+ *
+ * @property startDestination The starting destination for the navigation.
+ */
+class AndroidNavigator(override val startDestination: Destination) : Navigator {
+
+    private val _navigationActions = Channel<NavigationAction>(Channel.RENDEZVOUS)
+    override val navigationActions = _navigationActions.receiveAsFlow()
+
+    private var lastAction: NavigationAction? = null
+
+    /**
+     * Handles navigation actions, ensuring that duplicate actions are not processed.
+     *
+     * @param action The navigation action to handle.
+     * @param block The block to execute with the given action.
+     */
+    private inline fun handleAction(action: NavigationAction, block: (NavigationAction) -> Unit) {
+        if (action != lastAction) {
+            lastAction = action.also(block)
+        }
+    }
+
+    /**
+     * Navigates to the specified destination with the given [NavOptions].
+     *
+     * @param destination The target [Destination].
+     * @param navOptions Additional navigation options.
+     */
+    override suspend fun navigateTo(destination: Destination, navOptions: NavOptions) {
+        handleAction(NavigationAction.NavigateTo(destination, navOptions)) {
+            _navigationActions.trySend(it).onFailure { error ->
+                Log.e("AndroidNavigator", "Failed to enqueue navigation action: $error")
+            }
+        }
+    }
+
+    /**
+     * Navigates up in the navigation stack.
+     */
+    override suspend fun navigateUp() {
+        handleAction(NavigationAction.NavigateUp) {
+            _navigationActions.trySend(it).onFailure { error ->
+                Log.e("AndroidNavigator", "Failed to enqueue navigate up action: $error")
+            }
+        }
+    }
+}
+
 
 /**
  * Composable function for remembering a [Navigator] instance.
