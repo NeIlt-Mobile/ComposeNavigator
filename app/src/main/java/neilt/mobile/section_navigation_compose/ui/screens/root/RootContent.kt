9b4b9effa07d7f7ffd91bdb791204e6f56e14f14
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-11-16 08:42:24 +0300

    feat(ui): integrate BottomNavigationBar with navigation destinations
    Enhanced `RootContent` with a `BottomNavigationBar` for navigation:
    - Implemented dynamic `BottomNavigationBar` populated with `RootViewModel`'s navigation items.
    - Added navigation graph to `NavHost`, defining `MainSection` with `HomeScreen`, `ExploreScreen`, and `ProfileScreen` destinations.
    - Configured `RootViewModel` to provide navigation items with labels, selected/unselected icons, and click actions.
    - Updated `strings.xml` with localized strings for navigation labels.

--- app/src/main/java/neilt/mobile/section_navigation_compose/ui/screens/root/RootContent.kt
+++ app/src/main/java/neilt/mobile/section_navigation_compose/ui/screens/root/RootContent.kt
@@ -5,7 +5,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import neilt.mobile.section_navigation_compose.ui.components.navigation.BottomNavigationBar
+import neilt.mobile.section_navigation_compose.ui.navigation.AndroidDestination
 import neilt.mobile.section_navigation_compose.ui.theme.AppTheme
 import org.koin.androidx.compose.koinViewModel
 
@@ -17,12 +21,27 @@ fun RootContent(
         val navController = rememberNavController()
         val navigator = viewModel.navigator
 
-        Scaffold { innerPadding ->
+        Scaffold(
+            bottomBar = {
+                BottomNavigationBar(
+                    navItems = viewModel.navigationItems,
+                    navController = navController
+                )
+            }
+        ) { innerPadding ->
             NavHost(
                 modifier = Modifier.padding(innerPadding),
                 navController = navController,
                 startDestination = navigator.startDestination
-            ) {}
+            ) {
+                navigation<AndroidDestination.MainSection>(
+                    startDestination = AndroidDestination.MainSection.HomeScreen
+                ) {
+                    composable<AndroidDestination.MainSection.HomeScreen> {}
+                    composable<AndroidDestination.MainSection.ExploreScreen> {}
+                    composable<AndroidDestination.MainSection.ProfileScreen> {}
+                }
+            }
         }
     }
 }
