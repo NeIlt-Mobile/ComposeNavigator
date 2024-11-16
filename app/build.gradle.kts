b8e247f533f190db160ceb4c2627a02434e087ec
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-11-16 08:05:08 +0300

    chore(build): update dependencies and enable Compose features
    Updated the Gradle configuration to include new dependencies and enable Compose features:
    - Added Koin dependencies for dependency injection.
    - Integrated Compose BOM and related libraries.
    - Enabled Compose-specific build features and options with compiler extensions.
    - Updated `libs.versions.toml` with new Koin and Compose library definitions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,30 @@ plugins {
 
     // Kotlin
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinCompose)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+dependencies {
+
+    // Navigation
+    implementation(project(":core:navigation"))
+
+    // Koin
+    implementation(platform(libs.koinBom))
+    implementation(libs.koinCore)
+    implementation(libs.koinAndroid)
+    implementation(libs.koinAndroidCompose)
+
+    // Compose
+    implementation(platform(libs.composeBom))
+
+    implementation(libs.composeUi)
+    implementation(libs.composeUiGraphics)
+    implementation(libs.composeUiTooling)
+
+    implementation(libs.composeNavigation)
+    implementation(libs.composeMaterial)
 }
 
 android {
@@ -13,10 +37,20 @@ android {
 
     defaultConfig {
         applicationId = "neilt.mobile.section_navigation_compose"
+
         minSdk = 24
         targetSdk = 35
+
         versionCode = 1
         versionName = "1.0.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     compileOptions {
